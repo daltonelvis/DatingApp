1d81266393b5266fdef78d370e2044da84076125
Dalton Dsouza <daltonelvis@gmail.com>
2022-01-30 19:20:18 +0000

    docker compose working checkin -works on http only

--- client/src/app/Services/account.service.ts
+++ client/src/app/Services/account.service.ts
@@ -1,31 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
-import {map} from 'rxjs/operators'
+import { map } from 'rxjs/operators'
 import { User } from '../Models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  //local vs kubernates
+  //baseurl = 'http://127.0.0.1:5001/api/'
+  baseurl = 'https://localhost:5001/api/'
 
-  baseurl='https://localhost:5001/api/'
+  private CurrentUserSource = new ReplaySubject<User>(1)
+  currentUser$ = this.CurrentUserSource.asObservable();
 
-  private CurrentUserSource =new ReplaySubject<User>(1)
-  currentUser$=this.CurrentUserSource.asObservable();
+  constructor(private http: HttpClient) { }
 
-  
-
-
-  constructor(private http:HttpClient) { }
-
-  login(model:any)
-  {
-    return this.http.post(this.baseurl+'account/login',model).pipe(
-      map((response:User)=>{
-        const user=response;
-        if(user){
-          localStorage.setItem('user',JSON.stringify(user));
+  login(model: any) {
+    return this.http.post(this.baseurl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
           this.CurrentUserSource.next(user);
         }
       })
@@ -33,23 +30,19 @@ export class AccountService {
     );
   }
 
-  logout()
-  {
+  logout() {
     localStorage.removeItem('user');
     this.CurrentUserSource.next(null);
   }
 
-  setcurrentuser(user:User)
-  {
+  setcurrentuser(user: User) {
     this.CurrentUserSource.next(user);
   }
 
-  register(model:any)
-  {
-    return this.http.post(this.baseurl+'account/register',model).pipe(
-      map((user:User)=> {
-        if(user)
-        {
+  register(model: any) {
+    return this.http.post(this.baseurl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.CurrentUserSource.next(user);
         }
